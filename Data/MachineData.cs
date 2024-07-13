d66ff382ae4c266c8900fdfcc511c752aed4264d
jakubjn <109978198+jakubjn@users.noreply.github.com>
2024-07-13 11:18:42 +0100

    New Website

---
+++ Data/MachineData.cs
@@ -0,0 +1,10 @@
+﻿namespace GymSystem.Data
+{
+    [Serializable]
+    public class MachineData
+    {
+        public string? machineName {get; set;}
+        public string? displayImageURL {get; set;}
+        public float utilization {get; set;}
+    }
+}
